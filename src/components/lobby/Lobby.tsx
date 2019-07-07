1e31f80e83638fe4ea9350d33a320e2288bb8c3e
Michael Yang <yang.michaelk@gmail.com>
2019-07-07 13:13:06 -0700

    Limit nick 1-7 chars

--- src/components/lobby/Lobby.tsx
+++ src/components/lobby/Lobby.tsx
@@ -12,16 +12,21 @@ interface LobbyPropsFromState {
   playerCount: number;
   playerListItems: any;
   playerData: Player;
+  playerList: Player[];
 }
 
 interface LobbyState {
   value: string;
+  tooltip: boolean;
 }
 
 class Lobby extends React.Component<LobbyPropsFromState, LobbyState> {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = { 
+      value: "",
+      tooltip: false
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -32,9 +37,15 @@ class Lobby extends React.Component<LobbyPropsFromState, LobbyState> {
   }
 
   public handleClick() {
-    if(this.state.value.trim().length >=1)
-    {
+    const { playerList } = this.props
+    const nick = this.state.value.trim()
+    const dupNick = playerList.findIndex(player => player.nickName === nick);
+    if(nick.length >= 1 && nick.length < 8 && dupNick === -1)
+    { 
       updateNickName(this.state.value.trim());
+      this.setState({ tooltip: false });
+    } else {
+      this.setState({ tooltip: true });
     }
   }
 
@@ -49,25 +60,30 @@ class Lobby extends React.Component<LobbyPropsFromState, LobbyState> {
     }
   }
 
+  public showNickTooltip() {
+    if(this.state.tooltip) {
+      return (<span className="Warning">Nickname must be unique and 1-7 letters</span>);
+    } 
+  }
+
   public render() {
     const { gameId, playerCount, playerListItems } = this.props;
     return (
       <div className="Lobby">
-        <h3><u>Game ID: {gameId}</u></h3>
+        <h3 style={{wordBreak:"break-all"}}><u>Game ID:<br/>{gameId}</u></h3>
         <h4>{playerCount} player(s) connected: </h4>
-        <br />
         <ul className="list-unstyled">{playerListItems}</ul>
         <br />
         <div>
-          <div className="input-group mb-3">
+          <div className="NickTooltip input-group mb-3">
             <input type="text" value={this.state.value} onChange={this.handleChange}
                    placeholder={this.getNick()} className="form-control" />
             <div className="input-group-append">
               <button type="button" className="NicknameButton btn btn-outline-secondary" onClick={this.handleClick}>Update Nickname</button>
             </div>
           </div>
+          {this.showNickTooltip()}
         </div>
-        <br />
         <div>
           <StartButton playerCount={playerCount} />
           <MenuButton />
